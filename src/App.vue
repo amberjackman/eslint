1038bfe874bfe1a5ecc08c32b144d07d44addaf5
aa a <awakekaw166@protonmail.ch>
2022-11-01 19:39:13 +0900

    eslint

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ message }}</h1>
+  <HelloWorld />
+</template>
+
+<script>
+
+import HelloWorld from '~/components/HelloWorld'
+
+export default {
+  data() {
+    return {
+      message: 'heelo vue!!!'
+    }
+  }
+}
+</script>
